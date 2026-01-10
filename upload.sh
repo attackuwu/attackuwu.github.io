4489b486fd12a0449c2bef1c7a08e02024426ccc
AttackUwU <attackuwu@users.noreply.github.com>
2026-01-10 17:05:46 +0500

    Авто-обновление сайта: Sat Jan 10 17:05:46 +05 2026

--- upload.sh
+++ upload.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-
-echo "🚀 Начинаю загрузку на GitHub..."
-
-echo "🔄 Проверяю обновления..."
 git pull origin main --rebase || true
-
 git add .
-
-echo "📦 Упаковываю файлы..."
 git commit -m "Авто-обновление сайта: $(date)"
-
-echo "☁️ Отправляю в облако..."
 git push -u origin main
-
-echo "✅ Готово! Сайт обновится через пару минут."
-echo "👉 https://attackuwu.github.io"
